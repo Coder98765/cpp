86c838bb562e18195d2f09f92b7883d6511db4b8
Coder98765 <114956146+Coder98765@users.noreply.github.com>
2022-10-11 22:02:32 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,18 @@
+// substr()
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	string s1 = "Geeks";
+
+	
+	string r = s1.substr(2, 3);
+
+
+	cout << "String is: " << r;
+
+	return 0;
+}
