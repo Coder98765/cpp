63aa5d80def24e9217dd221fa5fbf4f3f5d97e6c
hacker1209382 <115182360+hacker1209382@users.noreply.github.com>
2022-10-11 22:23:06 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int toDigit(char ch)
+{
+	return (ch - '0');
+}
+
+
+int sumOfSubstrings(string num)
+{
+	int n = num.length();
+
+
+	int sumofdigit[n];
+
+	
+	sumofdigit[0] = toDigit(num[0]);
+	int res = sumofdigit[0];
+
+	
+	for (int i = 1; i < n; i++) {
+		int numi = toDigit(num[i]);
+
+		
+		sumofdigit[i] = (i + 1) * numi + 10 * sumofdigit[i - 1];
+
+	
+		res += sumofdigit[i];
+	}
+
+	return res;
+}
+
+int main()
+{
+	string num = "1226734";
+	cout << sumOfSubstrings(num) << endl;
+	return 0;
+}
