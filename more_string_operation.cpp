bdebe367d1303059f15d3ec9215b9ed928c8f25c
hacker1209382 <115182360+hacker1209382@users.noreply.github.com>
2022-10-11 22:05:41 +0530

    Create more_string_operation.cpp

---
+++ more_string_operation.cpp
@@ -0,0 +1,21 @@
+//print all substrings
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void subString(string s, int n)
+{
+	
+	for (int i = 0; i < n; i++)
+		for (int len = 1; len <= n - i; len++)
+			cout << s.substr(i, len) << endl;
+}
+
+
+int main()
+{
+	string s = "qcd";
+	subString(s, s.length());
+	return 0;
+}
